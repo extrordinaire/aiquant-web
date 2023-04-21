178cbf9a7faec00d391eb81cd973fbc87ce39f22
Maximo Angel Verzini Davico <maximoverzini@gmail.com>
2023-04-21 08:40:43 -0300

    fix(mobile_menu): fixed lang switch toggle in menu

--- src/app/sidenavmobile/sidenavmobile.component.ts
+++ src/app/sidenavmobile/sidenavmobile.component.ts
@@ -1,43 +1,51 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 declare var $: any;
 
 @Component({
-  selector: 'app-sidenavmobile',
-  templateUrl: './sidenavmobile.component.html',
-  styleUrls: ['./sidenavmobile.component.scss']
+  selector: "app-sidenavmobile",
+  templateUrl: "./sidenavmobile.component.html",
+  styleUrls: ["./sidenavmobile.component.scss"],
 })
 export class SidenavmobileComponent implements OnInit {
+  currentLang: string = "";
 
   @Output() childToParent = new EventEmitter();
 
-  constructor(
-    public translate: TranslateService
-  ) { }
-
-  ngOnInit(): void {
+  constructor(public translate: TranslateService) {
+    this.currentLang = translate.defaultLang;
   }
 
+  ngOnInit(): void {}
+
   open() {
-    document.getElementById('SideNavMobile')!.style.display = 'block';
+    document.getElementById("SideNavMobile")!.style.display = "block";
   }
 
   close() {
-    document.getElementById('SideNavMobile')!.style.display = 'none';
+    document.getElementById("SideNavMobile")!.style.display = "none";
   }
 
   static openRemote() {
-    document.getElementById('SideNavMobile')!.style.display = 'block';
+    document.getElementById("SideNavMobile")!.style.display = "block";
   }
 
-  goTo(indexPage?: any){
+  goTo(indexPage?: any) {
     this.close();
     this.childToParent.emit(indexPage);
   }
 
   useLanguage(language: string): void {
     this.translate.use(language);
-    document.getElementById('SideNavMobile')!.style.display = 'none';
+    this.translate.onLangChange.subscribe((response: any) => {
+      this.currentLang = response.lang;
+    });
+    document.getElementById("SideNavMobile")!.style.display = "none";
   }
-
 }
