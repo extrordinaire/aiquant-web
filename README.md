e3ef0ad033cb3d3ed6f06bf332aa979ac59cbc6e
Maximo Angel Verzini Davico <maximoverzini@gmail.com>
2023-04-21 08:26:31 -0300

    fix(visual artifacts): resolved completely scaling issues and the lateral drawer

--- README.md
+++ README.md
@@ -1,170 +1,76 @@
-# aiquant
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.3.
-
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-
-### Notas de finalización de mantenimiento por parte de Maximo Angel Verzini Davico 1/07/2022
-
-Análisis de la topología del sitio:
-
-```js
-<app-sidenavmobile (childToParent)="goToPage($event)"></app-sidenavmobile>
-<app-sidenav id="PRINCIPALSIDENAV" [optStyle]="colorSideNav" (childToParent)="goToPage($event)"
-  [ngClass]="{NavBarOpenSideNav: navBarOpenSideNav == true}"></app-sidenav>
-<div id="SliderPage" class="swiper-container" [swiper]="config" (indexChange)="onIndexChange($event)">
-  <div class="swiper-wrapper">
-    <!-- SECCION 1  -->
-    ...
-    <!-- SECCION 2  -->
-    ...
-    <!-- SECCION 3  -->
-    ...
-    <!-- SECCION 4  -->
-    <!-- ... -->
-    <!-- SECCION 5  -->
-    ...
-    <!-- SECCION 6  -->
-    <!-- ... -->
-    <!-- SECCION 7  -->
-    ...
-    <!-- SECCION 8  -->
-    ...
-    <!-- SECCION 9  -->
-    ...
-    <!-- SECCION 10  -->
-    ...
-  </div>
-
-  <div class="swiper-scrollbar" [hidden]="config.scrollbar === false"></div>
-  <div class="swiper-pagination" [hidden]="config.pagination === false"></div>
-
-  <!-- <div class="swiper-button-prev" [hidden]="config.navigation === false"></div>
-  <div class="swiper-button-next" [hidden]="config.navigation === false"></div> -->
+<div align="center">
+  pepe
 </div>
-```
 
-Tenemos un sólo archivo donde se encuentran definidas todas las secciones de la página, no se emplea correctamente la filosofía de diseño atómico, recomiendo fuertemente avanzar hacia un diseño más prolijo.
+# AIQuant-web
 
-> Puede que estos problemas sean algo del framework, desconozco acerca de las decisiones que tomó el desarrollador a la hora de planificar la página
+This is the website of [AIQuant](https://www.aiquantcapital.com), a platform that uses artificial intelligence to predict market trends.
 
-Cosas pendientes
+## Getting Started
 
-```jsx
+To get started with this project, follow these steps:
 
-...
-    <!-- SECCION 9  -->
-    <div class="swiper-slide" style="z-index: 99;">
-      <div class="SliderContent" fxLayout="column" fxLayoutAlign="center center" fxFlexFill>
-        <mat-sidenav-container class="example-container">
-          <mat-sidenav #sidenav mode="over" position="end" (closed)="sideNavClosed()">
-            <div class="CloseSideNav" (click)="closeSideNav()">
-              <span><img src="/assets/img/close-icon.svg" alt="" /></span>
-            </div>
-            <div class="SliderContentItems">
-              <div class="SideNavItem" style="border-bottom: 1px solid #FFF;">
-                <div class="SideNavImg">
-                  <img src="/assets/img/img5.png" alt="">
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'es'">
-                  <h4>{{currentContentSideNav[0].title}}</h4>
-                  <div>{{currentContentSideNav[0].content}}</div>
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'en'">
-                  <h4>{{currentContentSideNavEn[0].title}}</h4>
-                  <div>{{currentContentSideNavEn[0].content}}</div>
-                </div>
-              </div>
-              <div class="SideNavItem" style="border-bottom: 1px solid #FFF;">
-                <div class="SideNavImg">
-                  <img src="/assets/img/img5.png" alt="">
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'es'">
-                  <h4>{{currentContentSideNav[1].title}}</h4>
-                  <div>{{currentContentSideNav[1].content}}</div>
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'en'">
-                  <h4>{{currentContentSideNavEn[1].title}}</h4>
-                  <div>{{currentContentSideNavEn[1].content}}</div>
-                </div>
-              </div>
-              <div class="SideNavItem">
-                <div class="SideNavImg">
-                  <img src="/assets/img/img5.png" alt="">
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'es'">
-                  <h4>{{currentContentSideNav[2].title}}</h4>
-                  <div>{{currentContentSideNav[2].content}}</div>
-                </div>
-                <div class="SideNavItemContent" *ngIf="currentLang == 'en'">
-                  <h4>{{currentContentSideNavEn[2].title}}</h4>
-                  <div>{{currentContentSideNavEn[2].content}}</div>
-                </div>
-              </div>
-            </div>
+1. Clone the repository:
 
-        ...
+   ```
+   git clone https://github.com/extrordinaire/aiquant-web
+   ```
 
-      </div>
-    </div>
-...
-```
+2. Install the dependencies:
 
-Centrar los contenedores internos puede resultar una tarea engorrosa, ya que hay que corregir la estructura, hay mucho ruido visual, y luego de eso ver los estilos. Recomendaría tener en cuenta tecnologías como Tailwind CSS para manejar estilos responsivos de una forma más sencilla.
+   ```
+   cd aiquant-web
+   pnpm install
+   ```
 
-> En proyectos de este estilo yo tomaría en cuenta herramientas diseñadas para facilitar el desarrollo del proyecto, por ejemplo Preact.js junto a Tailwind y Sass, de esa forma evitar tanto código boilerplate.
+3. Run the development server:
 
-```jsx
+   ```
+   pnpm ng serve
+   ```
 
-/*
-    Ejemplo con Preact y Deno con el Framework Fresh.
-    .routes/index.tsx...
-*/
+4. Open the website in your browser:
 
-//** @jsx h */
-import { h } from "preact";
-import { tw } from "@twind";
-import pages from "../pages/pages.tsx";
+   ```
+   http://localhost:4200/
+   ```
 
-export default function Home() {
-  return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      { pages.map(page => return (page)) }
-    </div>
-  );
-}
+## Building for Production
+
+To build the website for production, run the following command:
 
 ```
+pnpm ng build --prod
+```
 
-Y cada página tendría una forma similar mínima.
-Como casi toda la página web está compuesta por elementos estáticos puede explotar la tecnología SSR con un framework FullStack como Fresh o el ya mencionado Preact, sirviendo al cliente HTML y CSS con el menor bundle de JavaScript posible, haciendo una página realmente ligera y rápida.
+This will compile the website and generate a set of static files that can be served by a web server.
 
-Recomiendo considerar el siguiente stack para futuros desarrollos web:
+## Contributing
 
-1. Next.js -> Solución fullStack basada en React.js con algunas mejoras
-2. React.js -> Uno de los líderes del desarrollo web, es una librería y compuesta por lo básico e indispensable, tiene un ecosistema gigante con soluciones para cualquier proyecto.
-3. Preact.js -> React hizo dieta, una solución más moderna mucho más ligera.
-4. Deno -> Tecnología de vanguardia. Alternativa a NodeJS con mejor rendimiento.
-5. Fresh.js -> Tecnología de vanguardia, Preact + Deno en un formato similar a Next.js
+If you would like to contribute to this project, please follow these guidelines:
+
+1. Fork the repository.
+
+2. Create a new branch for your feature or bug fix:
+
+   ```
+   git checkout -b my-feature
+   ```
+
+3. Make your changes and commit them:
+
+   ```
+   git commit -m "Added a new feature"
+   ```
+
+4. Push your changes to your fork:
+
+   ```
+   git push origin my-feature
+   ```
+
+5. Create a pull request and describe your changes in detail.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,162 +1,166 @@
-import { ViewChild } from '@angular/core';
-import { Component, HostListener } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { SwiperComponent, SwiperDirective, SwiperConfigInterface, SwiperPaginationInterface, SwiperScrollbarInterface } from 'ngx-swiper-wrapper'; // ADD swiperDirective
+import { ViewChild } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
+import { MatSidenav } from "@angular/material/sidenav";
+import {
+  SwiperComponent,
+  SwiperDirective,
+  SwiperConfigInterface,
+  SwiperPaginationInterface,
+  SwiperScrollbarInterface,
+} from "ngx-swiper-wrapper"; // ADD swiperDirective
 declare var $: any;
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-
 export class AppComponent {
   @ViewChild(SwiperDirective) directiveRef?: SwiperDirective;
-  @ViewChild('sidenav') sidenav!: MatSidenav;
+  @ViewChild("sidenav") sidenav!: MatSidenav;
 
   currentPage: number = 0;
-  title = 'leváre';
+  title = "leváre";
   currentPosition = window.pageYOffset;
   scrollBlock = true;
-  colorSideNav = 'white';
+  colorSideNav = "white";
   currentContentSideNav: any;
-  currentContentSideNavEn: any;
   optionSidenav: Number = 0;
   navBarOpenSideNav: boolean = false;
-  currentLang: string = ''
-  contentSideNav = [
+  currentLang: string = "";
+  contentSideNav: Array<
+    Array<{
+      titleES: string;
+      titleEN: string;
+      contentES: string;
+      contentEN: string;
+    }>
+  > = [
     [
       {
-        title: 'Ciclo de innovación',
-        content: 'La tecnología blockchain se encuentra actualmente en una etapa muy incipiente con potencial para un gran crecimiento, a medida que aumenta su adopción, también lo hará su utilidad y valor intrínseco.'
-      }, {
-        title: 'Nueva clase de activo financiero',
-        content: 'Los criptoactivos son cada vez mas reconocidos como una nueva clase de activo financiero por las principales instituciones y administradores de inversion.'
+        titleES: "Ciclo de innovación",
+        contentES:
+          "La tecnología blockchain se encuentra actualmente en una etapa muy incipiente con potencial para un gran crecimiento, a medida que aumenta su adopción, también lo hará su utilidad y valor intrínseco.",
+        titleEN: "Innovation cycle",
+        contentEN:
+          "The blockchain technology is currently at a very early stage with potential for huge growth, as its adoption increases so will its utility and intrinsic value.",
       },
       {
-        title: 'Diversificación',
-        content: 'Dada su nula correlación con activos financieros tradicionales, los criptoactivos son un instrumento financiero ideal para incluir en un portafolio de inversión.'
-      }
+        titleES: "Nueva clase de activo financiero",
+        contentES:
+          "Los criptoactivos son cada vez mas reconocidos como una nueva clase de activo financiero por las principales instituciones y administradores de inversion.",
+        titleEN: "New asset class",
+        contentEN:
+          "Cryptocurrencies are starting to get recognized as a new asset class by the main institutions and financial players",
+      },
+      {
+        titleES: "Diversificación",
+        contentES:
+          "Dada su nula correlación con activos financieros tradicionales, los criptoactivos son un instrumento financiero ideal para incluir en un portafolio de inversión.",
+        titleEN: "Diversification",
+        contentEN:
+          "Given its lack of correlation with the traditional financial assets, digital assets are an ideal instrument to include in a portfolio.",
+      },
     ],
     [
       {
-        title: 'Maximización de retorno',
-        content: 'A través de la diligente incorporación de marcos desarrollados internamente que logran alfa con riesgo moderado.'
-      }, {
-        title: 'Administración de portfolio',
-        content: 'Se busca superar el rendimiento de los mayores indices de activos digitales publicados por S&P y MVIS.'
+        titleES: "Maximización de retorno",
+        contentES:
+          "Ejecutamos estrategias de inversión eficientemente y sin emociones, esto garantiza una ejecución rápida y precisa.",
+        titleEN: "Return Maximization",
+        contentEN:
+          "We execute investment strategies efficiently and without emotions, ensuring fast and accurate execution.",
       },
       {
-        title: 'Asignación del fondo',
-        content: 'Dos tercios de las inversiones se asignan estratégicamente en posiciones que generan rentabilidad en el mediano y largo plazo, el otro tercio se invierte para lograr ganancias acumulativas en el corto y mediano plazo.'
-      }
+        titleES: "Administración de portfolio",
+        contentES:
+          "Monitoreamos y evaluamos tendencias en tiempo real, optimizando el desempeño vs el benchmark.",
+        titleEN: "Portfolio Administration",
+        contentEN:
+          "We monitor and evaluate trends in real-time, optimizing performance vs the benchmark.",
+      },
+      {
+        titleES: "Asignación del fondo",
+        contentES:
+          "Nuestros sistemas aprenden y se adaptan continuamente a medida que analizan grandes volúmenes de datos.",
+        titleEN: "Portfolio Balancing",
+        contentEN:
+          "Our systems continuously learn and adapt as they analyze large volumes of data.",
+      },
     ],
     [
       {
-        title: 'Análisis fundamental',
-        content: 'Estudio exhaustivo sobre la organización, el atractivo del mercado, la propuesta de valor, las finanzas y tokenomics, los plazos de implementación y el análisis del código.'
-      }, {
-        title: 'Análisis técnico',
-        content: 'Análisis grafico con el fin de optimizar el momento de compra o venta.'
+        titleES: "Análisis coyuntural",
+        contentES:
+          "Este proceso implica el seguimiento de las noticias y eventos que pueden afectar el mercado. Los algoritmos cuentan con técnicas de procesamiento de lenguaje natural y aprendizaje automático para analizar grandes cantidades de datos, como noticias, blogs y redes sociales, para identificar tendencias y patrones.",
+        titleEN: "Fundamental Analysis",
+        contentEN:
+          "Comprehensive study about the organisation, the attractiveness of the market, the value proposition, the financials and tokenomics, the timeline for implementation and code analysis.",
       },
       {
-        title: 'Análisis cuantitativo',
-        content: 'Herramientos de ponderaicon de activos que permiten equilibrar el riesgo y el rendimiento de nuestra cartera general'
-      }
-    ]
-  ]
-
-  contentSideNavEn = [
-    [
-      {
-        title: 'Innovation cycle',
-        content: 'The blockchain technology is currently at a very early stage with potential for huge growth, as its adoption increases so will its utility and intrinsic value.'
-      }, {
-        title: 'New asset class',
-        content: 'Cryptocurrencies are starting to get recognized as a new asset class by the main institutions and financial players'
+        titleES: "Análisis técnico",
+        contentES:
+          "Este proceso implica el uso de herramientas técnicas para analizar los datos históricos y actuales del mercado. Se utilizan indicadores técnicos para predecir el comportamiento futuro del mercado y para identificar oportunidades de compra y venta.",
+        titleEN: "Technical Analysis",
+        contentEN:
+          "Graphic analysis to optimize the moment to buy and sell each digital asset.",
       },
       {
-        title: 'Diversification',
-        content: 'Given its lack of correlation with the traditional financial assets, digital assets are an ideal instrument to include in a portfolio.'
-      }
+        titleES: "Análisis cuantitativo",
+        contentES:
+          "Este proceso implica el uso de modelos matemáticos y estadísticos para evaluar la rentabilidad de las inversiones.",
+        titleEN: "Quantitative Analysis",
+        contentEN:
+          "Asset weighting tools that allow us to balance the risk and reward of our portfolio.",
+      },
     ],
-    [
-      {
-        title: 'Return Maximization',
-        content: 'Through the diligent incorporation of in-house developed frameworks that achieve alpha while mitigating for risk'
-      }, {
-        title: 'Portfolio Administration',
-        content: 'We seek to outperform the largest digital asset indices published by S&P and MVIS.'
-      },
-      {
-        title: 'Portfolio Balancing',
-        content: 'Two thirds of the investments are strategically allocated in positions that yield return in the medium to long term, the other third is traded for cumulative gains in the short to medium term.'
-      }
-    ],
-    [
-      {
-        title: 'Fundamental Analysis',
-        content: 'Comprehensive study about the organisation, the attractiveness of the market, the value proposition, the financials and tokenomics, the timeline for implementation and code analysis.'
-      }, {
-        title: 'Technical Analysis',
-        content: 'Graphic analysis to optimize the moment to buy and sell each digital asset.'
-      },
-      {
-        title: 'Quantitative Analysis',
-        content: 'Asset weighting tools that allow us to balance the risk and reward of our portfolio.'
-      }
-    ]
-  ]
-
+  ];
 
   public config: SwiperConfigInterface = {
-    direction: 'vertical',
+    direction: "vertical",
     slidesPerView: 1,
     keyboard: true,
     mousewheel: true,
     scrollbar: false,
     navigation: true,
     pagination: false,
-    speed: 1000
+    speed: 1000,
   };
 
   private scrollbar: SwiperScrollbarInterface = {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
     hide: false,
-    draggable: true
+    draggable: true,
   };
 
   private pagination: SwiperPaginationInterface = {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
-    hideOnClick: false
+    hideOnClick: false,
   };
 
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'es']);
-    translate.setDefaultLang('en');
+    translate.addLangs(["en", "es"]);
+    translate.setDefaultLang("en");
 
     this.currentLang = translate.defaultLang;
-    this.translate.onLangChange.subscribe(
-      (response: any) => {
-        this.currentLang = response.lang;
-      }
-    )
+    this.translate.onLangChange.subscribe((response: any) => {
+      this.currentLang = response.lang;
+    });
   }
 
   ngOnInit(): void {
-    this.currentContentSideNav = this.contentSideNav[0];
-    this.currentContentSideNavEn = this.contentSideNavEn[0];
+    this.currentContentSideNav = this.contentSideNav[0][0].titleEN;
   }
 
   public onIndexChange(index: number) {
     this.currentPage = index;
-    console.log('Swiper index: ', index);
+    console.log("Swiper index: ", index);
     if (index == 1 || index == 3 || index == 4 || index == 6 || index == 8) {
-      this.colorSideNav = 'black';
+      this.colorSideNav = "black";
     } else {
-      this.colorSideNav = 'white';
+      this.colorSideNav = "white";
     }
   }
 
@@ -165,10 +169,9 @@ export class AppComponent {
   }
 
   openSideNav(opt: any) {
-    console.log('opt', opt);
+    console.log("opt", opt);
 
     this.currentContentSideNav = this.contentSideNav[opt];
-    this.currentContentSideNavEn = this.contentSideNavEn[opt];
     this.sidenav.open();
     this.navBarOpenSideNav = true;
   }
